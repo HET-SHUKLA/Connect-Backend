74fce4280f66994f3b4f448c1b7dfeade65e959b
HET <shuklahet2704@gmail.com>
2026-03-30 21:08:13 +0530

    Create transport.ts

---
+++ src/mediasoup/transport.ts
@@ -0,0 +1,20 @@
+import mediasoup from "mediasoup"
+import type { WebRtcTransport } from "mediasoup/types";
+
+const transports: Map<string, mediasoup.types.WebRtcTransport> = new Map()
+
+export function addTransport(transportId: string, transport: WebRtcTransport) {
+    transports.set(transportId, transport);
+}
+
+export function getTransport(transportId: string): WebRtcTransport {
+    const transport = transports.get(transportId);
+    if (!transport) {
+        throw new Error(`Transport ${transportId} not found`);
+    }
+    return transport;
+}
+
+export function removeTransport(transportId: string): void {
+    transports.delete(transportId);
+}
